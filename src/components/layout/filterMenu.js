1dff004986f748b74ddf7ae2d9f87004695c9113
JessieChiu1 <jessiechiu12@gmail.com>
2024-01-22 16:26:05 -0500

    filter menu changed to filter tree structure

--- src/components/layout/filterMenu.js
+++ src/components/layout/filterMenu.js
@@ -1,38 +1,79 @@
-import React from 'react'
-import { Menu, MenuTrigger, MenuPopover, MenuList, MenuItemLink, Button, Text, Subtitle2, makeStyles, shorthands } from "@fluentui/react-components"
+import React from 'react';
+import { Menu, MenuTrigger, MenuPopover, MenuList, MenuItemLink, Button, Text, Subtitle2, makeStyles, shorthands, MenuItem } from "@fluentui/react-components";
 
-const optionList = ["Outfit Showcase","Activewear", "Coats", "Jackets & Blazers", "Suits & Separates", "Dresses", "Jeans & Denim", "Loungewear", "Pants & Leggings", "Skirts", "Sleepwear", "Sweaters", "Sweatshirts & Hoodies", "Swimsuits & Cover-Ups", "T-Shirt and Tops", "Shoes", "Accessory", "Underwear", "Bra", "Shorts", "Swimwear", "Dress Shirt", "Blouses", "Jumpsuits & Rompers"]
-
+const optionList = [
+	"Accessory",
+	"Activewear",
+	{
+		label: "Bottom",
+		Androgynous: ["Dress Pants", "Jeans & Denim", "Pants & Leggings", "Shorts", "Skirts"],
+		Feminine: ["Dress Pants", "Jeans & Denim", "Pants & Leggings", "Shorts", "Skirts"],
+		Masculine: ["Dress Pants", "Jeans & Denim", "Pants & Leggings", "Shorts"],
+	},
+	"Loungewear",
+	"Outfit Showcase",
+	"Shoes",
+	"Sleepwear",
+	"Swimwear",
+	{
+		label: "Top",
+		Androgynous: ["Blouses", "Coats", "Dresses", "Dress Shirt", "Jackets & Blazers", "Suits & Separates", "Sweaters", "Sweatshirts & Hoodies", "T-Shirt"],
+		Feminine: ["Blouses", "Coats", "Dresses", "Jackets & Blazers", "Suits & Separates", "Sweaters", "Sweatshirts & Hoodies", "T-Shirt"],
+		Masculine: ["Coats", "Dress Shirt", "Jackets & Blazers", "Suits & Separates", "Sweaters", "Sweatshirts & Hoodies", "T-Shirt"],
+	},
+	{
+		label: "Undergarments",
+		Androgynous: ["Underwear", "Bra"],
+		Feminine: ["Underwear", "Bra"],
+		Masculine: ["Underwear"],
+	},
+]
 
 const useStyles = makeStyles({
-    button: {
-        ...shorthands.padding("15px")
-    }
-})
+button: {
+	...shorthands.padding("15px")
+}
+});
 
 export default function FilterMenu({ style }) {
-    const styles = useStyles();
+    const styles = useStyles()
 
     return (
-        <Menu>
-            <MenuTrigger disableButtonEnhancement>
-                <Button appearance='transparent' className={styles.button}>
+		<Menu>
+			<MenuTrigger disableButtonEnhancement>
+				<Button appearance='transparent' className={styles.button}>
                     <Text size={600}>{style}</Text>
                 </Button>
-            </MenuTrigger>
-    
-            <MenuPopover>
-                <MenuList>
-                    <MenuItemLink href={`${process.env.NEXT_PUBLIC_API_URL}/${style.toLowerCase()}`} key="All">
-                        <Subtitle2>All</Subtitle2>
-                    </MenuItemLink>
-                    {optionList.map((option) => (
-                        <MenuItemLink href={`${process.env.NEXT_PUBLIC_API_URL}/${style.toLowerCase()}?type=${option}`} key={option}>
-                            <Subtitle2>{option}</Subtitle2>
-                        </MenuItemLink>
-                    ))} 
-                </MenuList>
-            </MenuPopover>
-        </Menu>
+			</MenuTrigger>
+			<MenuPopover>
+				<MenuList>
+					<MenuItemLink href={`${process.env.NEXT_PUBLIC_API_URL}/${style.toLowerCase()}`} key="All">
+						<Subtitle2>All</Subtitle2>
+					</MenuItemLink>
+					{optionList.map((option) => (
+						option.label ? (
+							<Menu>
+								<MenuTrigger disableButtonEnhancement>
+									<MenuItem><Subtitle2>{option.label}</Subtitle2></MenuItem>
+								</MenuTrigger>
+								<MenuPopover>
+									<MenuList>
+										{option[style]?.map((item) => (
+											<MenuItemLink href={`${process.env.NEXT_PUBLIC_API_URL}/${style.toLowerCase()}?type=${item}`} key={item}>
+											<Subtitle2>{item}</Subtitle2>
+										</MenuItemLink>
+										))}
+									</MenuList>
+								</MenuPopover>
+							</Menu>
+						) : (
+							<MenuItemLink href={`${process.env.NEXT_PUBLIC_API_URL}/${style.toLowerCase()}?type=${option}`} key={option}>
+								<Subtitle2>{option}</Subtitle2>
+							</MenuItemLink>
+						)
+					))}
+				</MenuList>
+			</MenuPopover>
+		</Menu>
     )
 }
