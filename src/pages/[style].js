a979863be03924e7d73e446ee335014806276c9f
JessieChiu1 <jessiechiu12@gmail.com>
2024-01-28 18:49:01 -0500

    refactored load more logic

--- src/pages/[style].js
+++ src/pages/[style].js
@@ -2,82 +2,76 @@ import PostCard from "@/components/postCard"
 import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
 import { usePostByStyle } from "@/hooks/usePost"
-import { makeStyles, shorthands, Spinner, Text } from "@fluentui/react-components"
+import { makeStyles, shorthands, Spinner, Text, Button } from "@fluentui/react-components"
 import { useRouter } from 'next/router'
 import { useState, useEffect } from "react"
 
+const { min } = Math
+
 const useStyles = makeStyles({
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		minHeight: "100vh",
-		...shorthands.padding("20px"),
-	},
-	text: {
-		marginTop: "20px",
-	},
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    ...shorthands.padding("20px"),
+  },
+  text: {
+    marginTop: "20px",
+  },
+  loadMoreButton: {
+    marginTop: "20px",
+  },
 })
 
 export default function PostList() {
-	const styles = useStyles()
-	const router = useRouter()
-	const { style, type } = router.query
-	const { allPost, isLoading } = usePostByStyle(style, type)
+  const styles = useStyles()
+  const router = useRouter()
+  const { style, type } = router.query
+  const { allPost, isLoading } = usePostByStyle(style, type)
+  const [indexShown, setIndexShown] = useState(0)
 
-	const [pagePost, setPagePost] = useState([])
-	const [currentPage, setCurrentPage] = useState(0)
+  useEffect(() => {
+    if (allPost.length < 10) {
+      setIndexShown(allPost.length)
+    } else {
+      setIndexShown(10)
+    }
+  }, [allPost])
 
+  const handleLoadMore = () => {
+    const newIndex = min(allPost.length, indexShown + 10)
+    setIndexShown(newIndex)
+  }
 
-	useEffect(() => {
-	// This will create a 2D array where each subArray contains all of the post for their respective page 
-	// [[0-9 post],[10-19 post], [20-29 post]]
-	const createSubPageArray = (allPost, postPerPage) => {
-		const output = []
-
-		for (let i = 0; i < allPost.length; i += postPerPage) {
-		const pageArray = allPost.slice(i, i + postPerPage)
-		output.push(pageArray)
-		}
-
-		setPagePost(output)
-	}
-
-	createSubPageArray(allPost, 10)
-	}, [allPost])
-
-	const handleLoadMore = () => {
-		setCurrentPage(currentPage + 1)
-	}
-
-	return (
-		<>
-		  <Header />
-		  <section className={styles.container}>
-			{isLoading ? (
-			  <Spinner size="large" />
-			) : pagePost.length > 0 ? (
-			  pagePost.slice(0, currentPage + 1).flat().map((post) => (
-				<PostCard size="large" key={post._id} post={post} />
-			  ))
-			) : (
-			  <Text size={500} className={styles.text}>
-				No posts here yet
-			  </Text>
-			)}
-			{pagePost.length > currentPage + 1 && (
-			  <Button
-				size="large"
-				appearance="primary"
-				className={styles.loadMoreButton}
-				onClick={handleLoadMore}
-			  >
-				Load More
-			  </Button>
-			)}
-		  </section>
-		  <Footer />
-		</>
-	  )
+  return (
+    <>
+      <Header />
+      <section className={styles.container}>
+        {isLoading ? (
+          <Spinner size="large" />
+        ) : allPost.length > 0 ? (
+          allPost.slice(0, indexShown + 1).flat().map((post) => (
+            <PostCard size="large" key={post._id} post={post} />
+          ))
+        ) : (
+          <Text size={500} className={styles.text}>
+            No posts here yet
+          </Text>
+        )}
+        {allPost.length > indexShown + 1 && (
+          <Button
+            size="large"
+            appearance="primary"
+            className={styles.loadMoreButton}
+            onClick={handleLoadMore}
+          >
+            Load More
+          </Button>
+        )}
+      </section>
+      <Footer />
+    </>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,72 +5,67 @@ import { makeStyles, shorthands, Spinner, Button, Text } from "@fluentui/react-c
 import Footer from "@/components/layout/footer"
 import { useState, useEffect } from "react"
 
+const { min } = Math
+
 const useStyles = makeStyles({
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		minHeight: "100vh",
-		...shorthands.padding("20px"),
-	},
-})
-  
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    ...shorthands.padding("20px"),
+  },
+  loadMoreButton: {
+    marginTop: "20px",
+  },
+});
 
 export default function Home() {
-	const styles = useStyles()
-	const { allPost, isLoading } = usePostByStyle("")
-	const [pagePost, setPagePost] = useState([])
-	const [currentPage, setCurrentPage] = useState(0)
+  const styles = useStyles()
+  const { allPost, isLoading } = usePostByStyle("")
+  const [indexShown, setIndexShown] = useState(0)
 
-	useEffect(() => {
-		// This will create a 2D array where each subArray contains all of the post for their respective page 
-		// [[0-9 post],[10-19 post], [20-29 post]]
-		const createSubPageArray = (allPost, postPerPage) => {
-			const output = []
-	
-			for (let i = 0; i < allPost.length; i += postPerPage) {
-			const pageArray = allPost.slice(i, i + postPerPage)
-			output.push(pageArray)
-			}
-	
-			setPagePost(output)
-		}
-	
-		createSubPageArray(allPost, 10)
-	}, [allPost])
-	
-	const handleLoadMore = () => {
-		setCurrentPage(currentPage + 1)
-	}
+  useEffect(() => {
+    if (allPost.length < 10) {
+      setIndexShown(allPost.length)
+    } else {
+      setIndexShown(10)
+    }
+  }, [allPost])
 
-	return (
-		<>
-		  <Header />
-		  <section className={styles.container}>
-			{isLoading ? (
-			  <Spinner size="large" />
-			) : pagePost.length > 0 ? (
-			  pagePost.slice(0, currentPage + 1).flat().map((post) => (
-				<PostCard size="large" key={post._id} post={post} />
-			  ))
-			) : (
-			  <Text size={500} className={styles.text}>
-				No posts here yet
-			  </Text>
-			)}
-			{pagePost.length > currentPage + 1 && (
-			  <Button
-				appearance="primary"
-				size="large"
-				className={styles.loadMoreButton}
-				onClick={handleLoadMore}
-			  >
-				Load More
-			  </Button>
-			)}
-		  </section>
-		  <Footer />
-		</>
-	)
+  const handleLoadMore = () => {
+    const newIndex = min(allPost.length, indexShown + 10)
+    setIndexShown(newIndex)
+  };
+
+  return (
+    <>
+      <Header />
+      <section className={styles.container}>
+        {isLoading ? (
+          <Spinner size="large" />
+        ) : allPost.length > 0 ? (
+          allPost.slice(0, indexShown).flat().map((post) => (
+            <PostCard size="large" key={post._id} post={post} />
+          ))
+        ) : (
+          <Text size={500} className={styles.text}>
+            No posts here yet
+          </Text>
+        )}
+        {allPost.length > indexShown && (
+          <Button
+            appearance="primary"
+            size="large"
+            className={styles.loadMoreButton}
+            onClick={handleLoadMore}
+          >
+            Load More
+          </Button>
+        )}
+      </section>
+      <Footer />
+    </>
+  )
 }
